717a8147189119fd3470459b432ed5d995386b97
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-03-14 19:34:39 +0100

    Added LoginReqiuredMixin and edited TaskCreateView

--- tasks/views.py
+++ tasks/views.py
@@ -3,19 +3,27 @@ from django.urls import reverse_lazy
 from tasks import models
 from django.views.generic import ListView, DetailView, CreateView
 from tasks.forms import TaskForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class TaskListView(ListView):
     model = models.Task
     context_object_name = "tasks"
     template_name = "tasks/task_list.html"
 
+
 class TaskDetailView(DetailView):
     model = models.Task
     context_object_name = "task"
     template_name = "tasks/task_detail.html"
 
-class TaskCreateView(CreateView):
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = models.Task
     template_name = "tasks/task_form.html"
     form_class = TaskForm
     success_url = reverse_lazy("tasks:taks-list")
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
