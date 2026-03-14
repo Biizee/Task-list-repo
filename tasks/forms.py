7b9abde4e6c9cb6d6819e5b00d03e1431102888c
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-03-14 19:29:07 +0100

    Edited form

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,4 +4,4 @@ from tasks.models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = {"title", "description", "status", "priority", "due_date", "creator"}
+        fields = {"title", "description", "status", "priority", "due_date"}
