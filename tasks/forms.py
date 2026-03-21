116fab504b2c3592b5b466283b0932e786f607de
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-03-21 19:19:13 +0100

    Added new custom form

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,3 +5,15 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = {"title", "description", "status", "priority", "due_date"}
+
+
+class TaskFilterForm(forms.Form):
+
+    STATUS_CHOICES = {
+        ("", "All")
+        ("todo", "To Do"),
+        ("in_progress", "In Progress"),
+        ("done", "Done")
+    }
+
+    status = forms.ChoiceField(choices=STATUS_CHOICES, required=False, label="Статус")
